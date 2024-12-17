73c53c6e94936731e4c243c8dec91c16cc069397
Tharindu37 <tharindut520@gmail.com>
2024-12-17 16:52:20 +0530

    CRUD Done

--- crud/crudAdminApp/urls.py
+++ crud/crudAdminApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name="posts"),
-    path('post/<str:pid>/', views.post, name="post")
+    path('post/<str:pid>/', views.post, name="post"),
+    path('create-form', views.createNewPost, name="create"),
+    path('upate-form/<str:pid>/',views.updatePost,name="update"),
+    path('delete/<str:pid>/', views.delete,name="delete")
 ]
--- crud/crudAdminApp/views.py
+++ crud/crudAdminApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import postTable
+from . form import createPost
 
 # Create your views here.
 def posts(request):
@@ -10,4 +11,32 @@ def posts(request):
 
 
 def post(request, pid):
-    return render(request, 'post.html')
+    database = postTable.objects.get(id=pid)
+    return render(request, 'post.html',{'database':database})
+
+def createNewPost(request):
+    form = createPost()
+    if request.method == "POST":
+        form = createPost(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('posts')
+    return render(request, ('form.html'),{'form':form})
+
+
+def updatePost(request,pid):
+    database = postTable.objects.get(id = pid)
+    form = createPost(instance=database)
+    if request.method == "POST":
+        form = createPost(request.POST, instance=database)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+    return render(request, 'form.html', {'form':form})
+
+def delete(request, pid):
+    database = postTable.objects.get(id=pid)
+    if request.method == "POST":
+        database.delete()
+        return redirect('posts')
+    return render(request, 'del.html')
